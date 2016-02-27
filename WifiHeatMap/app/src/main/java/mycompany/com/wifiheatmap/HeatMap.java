b9840843479a0d07c04b64353f363c238f348690
baehre <baehre@vt.edu>
2016-02-27 00:54:22 -0500

    end of the night push

--- WifiHeatMap/app/src/main/java/mycompany/com/wifiheatmap/HeatMap.java
+++ WifiHeatMap/app/src/main/java/mycompany/com/wifiheatmap/HeatMap.java
@@ -5,48 +5,88 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class HeatMap extends AppCompatActivity implements SensorEventListener{
 
     private SensorManager sensorManager;
-    //for calculating a step
-    private float previousZ;
-    private float currentZ;
-    //how sensitive we are for checking footstep
-    private int threshold;
-    //for counting steps
-    private int numSteps;
-    //grabbing accelerometer values
+    //grabbing compass azimuth
     private float[] gravity;
     private float[] geoMagnetic;
 
     private float azimuth;
-    private double azimuthDouble;
+    private int feet;
+    private int inches;
+    private int height;
+    private double stepLength;
+    private boolean walking;
 
-    private TextView stepNum;
-    private TextView angle;
+    //x and y coordinate of person
+    private int x;
+    private int y;
 
+    //like a clock north is 0. includes non cardinals (i.e. NE)
+    private int direction;
+
+    private TextView angleText;
+    private Button startButton;
+    private Spinner feetSpinner;
+    private Spinner inchesSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heat_map);
         //initialize values
-        threshold = 5;
-        numSteps = 0;
-        previousZ = 0;
-        currentZ = 0;
-        stepNum = (TextView)findViewById(R.id.stepNum);
-        angle = (TextView)findViewById(R.id.angle);
-        stepNum.setText("Number of Steps: 0");
+        direction = 0;
+        walking = false;
+
+        angleText = (TextView)findViewById(R.id.angle);
+        startButton = (Button)findViewById(R.id.startButton);
+        feetSpinner = (Spinner)findViewById(R.id.feet);
+        inchesSpinner = (Spinner)findViewById(R.id.inches);
+
+        startButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(startButton.getText() == "Start"){
+                    startButton.setText("Stop");
+                    feet = (int)feetSpinner.getSelectedItem();
+                    inches = (int)inchesSpinner.getSelectedItem();
+                    height = feet * 12 + inches;
+                    stepLength = 0.415 * height;
+                    walking = true;
+                    startWalk();
+                }
+                else if(startButton.getText() == "Stop"){
+                    startButton.setText("Start");
+                    walking = false;
+                }
+            }
+        });
         initListening();
+    }
+
+    private void startWalk(){
+        long last = System.currentTimeMillis();
+        while (walking) {
+            long current = System.currentTimeMillis();
+            if(current-last>=1000){
+                WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+                int wifiStrength = getWifiStrength(wifi);
+                last = current;
+            }
+        }
     }
 
     private void initListening(){
@@ -61,18 +101,17 @@ public class HeatMap extends AppCompatActivity implements SensorEventListener{
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    private int getWifiStrength(WifiManager wifiManager){
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int level = wifiInfo.getRssi();
+        return level;
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         switch(event.sensor.getType()){
             case Sensor.TYPE_ACCELEROMETER:
                 gravity = event.values;
-                float z = gravity[2];
-                currentZ = z;
-                if(Math.abs(currentZ - previousZ) > threshold){
-                    numSteps++;
-                    stepNum.setText("Number of Steps: " + numSteps);
-                }
-                previousZ = z;
                 break;
             case Sensor.TYPE_MAGNETIC_FIELD:
                 geoMagnetic = event.values;
@@ -88,10 +127,45 @@ public class HeatMap extends AppCompatActivity implements SensorEventListener{
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(R, orientation);
                 azimuth = orientation[0];
-                azimuthDouble = Math.toDegrees(azimuth);
-                String output = String.format("%.2f", azimuthDouble);
-                angle.setText("Angle(Degrees): " + output);
+                azimuth = -azimuth*360/(2*3.14159265358979323f);
+                String output = String.format("%.2f", azimuth);
+                angleText.setText("Angle(Degrees): " + output);
+                checkDirection();
             }
+        }
+    }
+
+    public void checkDirection(){
+        float tempAzimuth;
+        if(azimuth < 0){
+            tempAzimuth = azimuth + 360;
+        }
+        else{
+            tempAzimuth = azimuth;
+        }
+        if(tempAzimuth<=22.5 || tempAzimuth>337.5){
+            direction = 0;
+        }
+        else if(tempAzimuth<=67.5 && tempAzimuth>22.5){
+            direction = 1;
+        }
+        else if(tempAzimuth<=112.5 && tempAzimuth>67.5){
+            direction = 2;
+        }
+        else if(tempAzimuth<=157.5 && tempAzimuth>112.5){
+            direction = 3;
+        }
+        else if(tempAzimuth<=202.5 && tempAzimuth>157.5){
+            direction = 4;
+        }
+        else if(tempAzimuth<=247.5 && tempAzimuth>202.5){
+            direction = 5;
+        }
+        else if(tempAzimuth<=292.5 && tempAzimuth>247.5){
+            direction = 6;
+        }
+        else if(tempAzimuth<=337.5 && tempAzimuth>292.5){
+            direction = 7;
         }
     }
 
